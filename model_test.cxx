90b5495a54fbd358c681317bfe955927c8933dfa
interstellar-spaces <dnguyen2048@gmail.com>
2020-06-03 19:36:25 -0400

    Add files via upload

--- model_test.cxx
+++ model_test.cxx
@@ -6,19 +6,25 @@ using namespace ge211;
 TEST_CASE("Initial platforms")
 {
     Model m;
-    std::vector<Platform> expected {
-            {{300, 100}},
-            {{0, 200}},
-            {{100, 300}},
-            {{0, 400}},
-            {{100, 500}},
-            {{0, 600}},
-            {{0, 700}}
-    };
-    CHECK(m.get_platforms() == expected);
+    std::vector<Platform> expected;
+    expected.push_back({{0, 530}});
+    expected.push_back({{50, 460}});
+    expected.push_back({{-100, 390}});
+    expected.push_back({{50, 320}});
+    expected.push_back({{-100, 250}});
+    expected.push_back({{50, 180}});
+    expected.push_back({{-100, 110}});
+
+    std::vector<Platform> actual = m.get_platforms();
+
+    CHECK(expected.size() == actual.size());
+
+    for (int i = 0; i < 7; i++) {
+        CHECK(actual[i].position == expected[i].position);
+    }
 }
 
-struct Test_access // 4
+struct Test_access
 {
     Model& model;
     std::vector<Barrel>& barrels()
@@ -32,48 +38,55 @@ struct Test_access // 4
 
 };
 
-TEST_CASE("Barrels fall through")
+
+TEST_CASE("Make 1 Barrel")
 {
-    Model m;
-    Test_access t{m };
-    Barrel test_barrel = {{0, 200}, true, false};
+    Model       m;
+    Test_access t{m};
+    Pos         p0          = {10, 94};
+    Barrel      test_barrel = {p0, {5, 0}, false};
     t.barrels().push_back(test_barrel);
-    test_barrel.move_to(500); // let {500, 200} be the location of the hole
-    m.update();
-    CHECK(test_barrel.barrel_falls());
+    m.update(1);
+    CHECK(m.get_barrels().size() == 1);
+    m.update(5);
+    CHECK(m.get_barrels().size() == 2);
 }
+//    test_barrel.move_to(500); // let {500, 200} be the location of the hole
+//    m.update();
+//    CHECK(test_barrel.barrel_falls());
+//}
 
-TEST_CASE("Special barrel climbs ladder")
-{
+//TEST_CASE("Special barrel climbs ladder")
+//{
 // checks if barrel climbs the ladder
-    Model m;
-    Test_access t{m };
-    Barrel special_barrel = {{100, 500}, true, true};
-    t.barrels().push_back(special_barrel);
-    t.ladder_.push_back({{200, 500}, false});
-    m.update();
-    special_barrel.move_to(lad.x);
-    CHECK(special_barrel.barrel_climbs_ladder());
-    m.update();
+//    Model m;
+//    Test_access t{m };
+//    Barrel special_barrel = {{100, 500}, true, true};
+//    t.barrels().push_back(special_barrel);
+//    t.ladder_.push_back({{200, 500}, false});
+//    m.update();
+//    special_barrel.move_to(lad.x);
+//    CHECK(special_barrel.barrel_climbs_ladder());
+//    m.update();
+//
+//    CHECK(m.get_barrels() == {{200, 600}, true, true});
+//}
 
-    CHECK(m.get_barrels() == {{200, 600}, true, true});
-}
-
-TEST_CASE("Player climbs ladder")
-{
-    Model m;
-    Test_access t{m };
-    m.ladder_.clear();
-    m.ladder_.push_back({{200, 700}, false});
-    player.move_to(200);
-    CHECK(player.player_climbs_ladder())
-}
-
-TEST_CASE("Player falls")
-{
-    Model m;
-    player.move_to(500) // let x = 500 be the position of the hole
-    CHECK(player.player_falls());
-
-}
+//TEST_CASE("Player climbs ladder")
+//{
+//    Model m;
+//    Test_access t{m };
+//    m.ladder_.clear();
+//    m.ladder_.push_back({{200, 700}, false});
+//    player.move_to(200);
+//    CHECK(player.player_climbs_ladder())
+//}
+//
+//TEST_CASE("Player falls")
+//{
+//    Model m;
+//    player.move_to(500) // let x = 500 be the position of the hole
+//    CHECK(player.player_falls());
+//
+//}
 
